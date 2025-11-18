64faee4ee72f473e6e58618085b3eb4d7d98b9ec
kcrypto126 <technt22@gmail.com>
2025-11-18 22:54:42 +0900

    feat: add babel, metro, tailwind config

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }], // Use nativewind as jsxImportSource
-      "nativewind/babel", // Add nativewind as a preset
-    ],
-    plugins: [
-      "expo-router/babel", // Ensure expo-router is in plugins array
-      "react-native-reanimated/plugin", // Add other plugins if you use them
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
-  presets: [require("nativewind/preset")], // Add the NativeWind preset
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
